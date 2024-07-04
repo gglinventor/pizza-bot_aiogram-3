aa139dccd29be04fb720cc439aa27b8246e1f134
gglinventor <143726984+gglinventor@users.noreply.github.com>
2024-07-04 16:28:11 +0300

    Add files via upload

---
+++ text_info/bot_commands.py
@@ -0,0 +1,5 @@
+from aiogram.types import BotCommand
+
+user_private = [BotCommand(command='start', description='Запуск бота'), BotCommand(command='help', description='Помощь'), BotCommand(command='info', description='Информация о боте')] #BotCommand(command='menu', description='Вывод меню')
+
+#admin_private = [BotCommand(command='admin_go', description='Запуск прав админа'), BotCommand(command='start', description='Запуск бота'), BotCommand(command='menu', description='Вывод меню'), BotCommand(command='help', description='Помощь')]
